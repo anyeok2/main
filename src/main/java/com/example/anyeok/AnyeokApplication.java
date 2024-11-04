92e13a82b5940cbe44dec9d0731846370f710e8b
anyeok <https://github.com/anyeok>
2024-11-04 12:21:08 +0900

    24.11.04 시작

---
+++ src/main/java/com/example/anyeok/AnyeokApplication.java
@@ -0,0 +1,13 @@
+package com.example.anyeok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyeokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyeokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/anyeok/AnyeokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anyeok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyeokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
